6d265a2a7c284f62123bf789779b6cc2f7939968
Heesu01 <rulru0413@naver.com>
2024-10-05 16:12:55 +0900

    setting: 테마 설정

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,7 +1,32 @@
-import React from "react";
+import { css } from "styled-components";
+import "./font.css";
 
-const Theme = () => {
-  return <div></div>;
+export const Theme = {
+  fonts: {
+    logo: css`
+      font-family: "HakgyoansimPuzzleTTF", sans-serif;
+      font-size: 30px;
+    `,
+    default: css`
+      font-family: "Pretendard", sans-serif;
+      font-size: 13px;
+    `,
+    title: css`
+      font-family: "Pretendard", sans-serif;
+      font-size: 35px;
+      font-weight: 600;
+    `,
+    other: css`
+      font-family: "", sans-serif;
+    `,
+  },
+  colors: {
+    white: "#fff",
+    black: "#000",
+    black2: "#333332",
+    gray: "#9A9A9A",
+    gray2: "#c4c4c4",
+    gray3: "#E9E9E9",
+  },
+  breakpoints: {},
 };
-
-export default Theme;
