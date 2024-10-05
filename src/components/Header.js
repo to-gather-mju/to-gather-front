14be6f7dff379922e46563b88570c1d0486622bc
Heesu01 <rulru0413@naver.com>
2024-10-05 17:02:55 +0900

    feat: 헤더 UI 구현

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Container>
+      <Logo>투게더</Logo>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid ${(props) => props.theme.colors.gray2};
+  padding: 0 30px;
+  justify-content: space-between;
+  color: ${(props) => props.theme.colors.red};
+`;
+
+const Logo = styled.div`
+  margin-right: 50px;
+  font-size: 30px;
+  ${(props) => props.theme.fonts.logo};
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default Header;
