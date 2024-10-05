77d4d28ee95797537b8ea04e723762ac7039948a
Heesu01 <rulru0413@naver.com>
2024-10-05 16:24:45 +0900

    setting: 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Calendar from "./pages/Calendar";
+import Home from "./pages/Home";
+import Complete from "./pages/Complete";
+import Location from "./pages/Location";
+import Result from "./pages/Result";
+import Time from "./pages/Time";
+import Title from "./pages/Title";
+import Vote from "./pages/Vote";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/main" />} />
+      <Route path="/main" element={<Home />} />
+      <Route path="/Calendar" element={<Calendar />} />
+      <Route path="/Complete" element={<Complete />} />
+      <Route path="/Location" element={<Location />} />
+      <Route path="/Result" element={<Result />} />
+      <Route path="/Time" element={<Time />} />
+      <Route path="/Title" element={<Title />} />
+      <Route path="/Time" element={<Vote />} />
+    </Routes>
+  );
 }
 
 export default App;
