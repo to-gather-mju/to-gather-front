b567c5db6fa28870da310afa1bcef0927ddb16a9
Heesu01 <rulru0413@naver.com>
2024-10-05 17:03:23 +0900

    feat: 레이아웃 app에 추가

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import Home from "./pages/Home";
 import Complete from "./pages/Complete";
 import Location from "./pages/Location";
 import Result from "./pages/Result";
-import Time from "./pages/Time";
+import TimeTable from "./pages/TimeTable";
 import Title from "./pages/Title";
 import Vote from "./pages/Vote";
+import Layout from "./components/Layout";
+import TimeSet from "./pages/TimeSet";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/main" />} />
-      <Route path="/main" element={<Home />} />
-      <Route path="/Calendar" element={<Calendar />} />
-      <Route path="/Complete" element={<Complete />} />
-      <Route path="/Location" element={<Location />} />
-      <Route path="/Result" element={<Result />} />
-      <Route path="/Time" element={<Time />} />
-      <Route path="/Title" element={<Title />} />
-      <Route path="/Time" element={<Vote />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to="/main" />} />
+        <Route path="/main" element={<Home />} />
+        <Route path="/Calendar" element={<Calendar />} />
+        <Route path="/Complete" element={<Complete />} />
+        <Route path="/Location" element={<Location />} />
+        <Route path="/Result" element={<Result />} />
+        <Route path="/TimeTable" element={<TimeTable />} />
+        <Route path="/TimeSet" element={<TimeSet />} />
+        <Route path="/Title" element={<Title />} />
+        <Route path="/Time" element={<Vote />} />
+      </Routes>
+    </Layout>
   );
 }
 
