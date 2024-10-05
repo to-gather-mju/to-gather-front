ee3fbf59cee5e57a10bb1ef5e395ee1a2898ef46
Heesu01 <rulru0413@naver.com>
2024-10-05 16:02:30 +0900

    setting: 글로벌 스타일 설정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
-const GlobalStyle = () => {
-  return <div></div>;
-};
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+
+  html, body, div, span, h1, h2, h3, h4, h5, h6, p,
+  a, dl, dt, dd, ol, ul, li, form, label, table, input, textarea, button {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    text-decoration: none;
+    box-sizing: border-box;
+    &:visited {
+      text-decoration: none;
+      color: black;
+    }
+  }
+  body {
+    line-height: 1;
+    margin: 0;
+    padding: 0;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  button {
+    border: 0;
+    cursor: pointer;
+  }
+`;
 
 export default GlobalStyle;
